66cb4fee8091b793b092382f1977a5f5c7d7959c
Rudolph Gottesheim <r.gottesheim@loot.at>
2011-12-17 16:30:26 +0100

    added manage users activity

--- src/at/yoerg/android/activity/Main.java
+++ src/at/yoerg/android/activity/Main.java
@@ -1,7 +1,9 @@
-package at.yoerg.android;
+package at.yoerg.android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import at.yoerg.android.R;
+import at.yoerg.android.R.layout;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
---
+++ src/at/yoerg/android/activity/ManagePlayers.java
@@ -0,0 +1,14 @@
+package at.yoerg.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ManagePlayers extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
