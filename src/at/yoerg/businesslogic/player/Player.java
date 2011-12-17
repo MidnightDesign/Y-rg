40f7225e1cc1f4a59bf231ece28c53ff57d2153d
Rudolph Gottesheim <r.gottesheim@loot.at>
2011-12-17 18:45:05 +0100

    implemented player

--- src/at/yoerg/businesslogic/player/Player.java
+++ src/at/yoerg/businesslogic/player/Player.java
@@ -1,5 +1,42 @@
 package at.yoerg.businesslogic.player;
 
+import java.util.UUID;
+
 public class Player {
+	
+	private String name;
+	private Sex sex;
+	private String id;
+	
+	protected Player() {
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
+
+	public Sex getSex() {
+		return sex;
+	}
+	
+	public static Player create(String name) {
+		Player player = new Player();
+		player.setName(name);
+		player.setSex(Sex.UNKNOWN);
+		player.id = UUID.randomUUID().toString();
+		return player;
+	}
+
+	public static enum Sex {
+		MALE, FEMALE, UNKNOWN
+	}
 
 }
